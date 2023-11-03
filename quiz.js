f012c9868692bd07c3714ea341db264fceb80db0
피치 <148326944+peachchy@users.noreply.github.com>
2023-11-04 01:38:43 +0900

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,26 @@
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+var myQuestions = [
+  {
+    question: "첫번째 문제",
+    answers: {
+      a: 'A 답변',
+      b: 'B 답변',
+      c: 'C 답변'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "두번째 문제",
+    answers: {
+      a: 'A 답변',
+      b: 'B 답변',
+      c: 'C 답변'
+    },
+    correctAnswer: 'b'
+  }
+];
+function showResults(){}
+function showSlide(){}
+function buildQuiz(){}
